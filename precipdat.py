ec9e1531e5cd6c298f9530fe160ae78fb27d5c08
Jennifer Walker <jenfly@gmail.com>
2015-10-13 11:32:33 -0700

    Added precipdat module to read precp datasets

---
+++ precipdat.py
@@ -0,0 +1,64 @@
+"""Functions to read various precipitation datasets.
+
+- CMAP
+- GPCP
+- TRMM
+"""
+
+import sys
+sys.path.append('/home/jwalker/dynamics/python/atmos-tools')
+
+from __future__ import division
+import xray
+import numpy as np
+from datetime import datetime
+import matplotlib.pyplot as plt
+import pandas as pd
+import atmos as atm
+
+def read_cmap(datafile, yearmin=None, yearmax=None):
+    """Read CMAP pentad data for selected years.
+
+    Parameters
+    ----------
+    datafile : str
+        File path for CMAP data in NetCDF format.
+    yearmin, yearmax : ints, optional
+        Min and max years (inclusive) to extract.
+
+    Returns
+    -------
+    precip : xray.DataArray
+        CMAP pentad data for selected years, reshaped to
+        [year, pentad, lat, lon].
+    """
+
+    NPYR = 73       # Number of pentads per year
+    YRMIN = 1979    # First year of CMAP data
+
+    if yearmin is None:
+        yearmin = YRMIN
+
+    # Read data
+    with xray.open_dataset(cmap_file) as ds:
+        precip = ds['precip'].load()
+
+    # Discard incomplete year at end
+    ny = precip.shape[0] // NPYR
+    precip = precip[:ny*NPYR]
+
+    # Split into individual years
+    years = np.arange(YRMIN, YRMIN + ny)
+    pentads = np.arange(1, NPYR + 1)
+    precip = atm.split_timedim(precip, NPYR, time0_name='year',
+                               time0_vals=years, time1_name='pentad',
+                               time1_vals=pentads)
+
+    # Extract selected years
+    if yearmax is None:
+        years = np.arange(yearmin, YRMIN + ny)
+    else:
+        years = np.arange(yearmin, yearmax + 1)
+    precip = precip.sel(year=years)
+
+    return precip
