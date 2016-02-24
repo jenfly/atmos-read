eec4b04a4e9fea767d01b2cfaf18c0e2d4889e86
Jennifer Walker <jenfly@gmail.com>
2016-02-24 14:31:13 -0800

    Housekeeping

--- scripts/merra-vert-int.py
+++ scripts/merra-vert-int.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append('/home/jwalker/dynamics/python/atmos-tools')
 sys.path.append('/home/jwalker/dynamics/python/atmos-read')
 
+import os
 import xray
 import numpy as np
 from datetime import datetime
@@ -12,60 +13,67 @@ import precipdat
 import merra
 
 # ----------------------------------------------------------------------
-# Download precipitable water
+# Download vertically integrated fluxes
 
-datadir = atm.homedir() + 'datastore/merra/daily/'
-years = np.arange(1979, 2015)
+version, years = 'merra2', np.arange(1980, 2016)
+# version, years = 'merra', np.arange(1979, 2016)
+
+varnms = ['UFLXQV', 'VFLXQV', 'DQVDT_ANA']
+plev = None
+
+datadir = atm.homedir() + 'datastore/' + version + '/daily/'
 months = np.arange(1, 13)
+lon1, lon2 = 40, 120
+lat1, lat2 = -90, 90
 
-# varnms = ['TQV']
-# vertical, res, time_kind, kind = 'X', 'N', 'I', 'INT'
-# varnms = ['UFLXCPT', 'VFLXCPT', 'UFLXPHI', 'VFLXPHI', 'UFLXQV', 'VFLXQV']
-varnms = ['DQVDT_ANA']
-vertical, res, time_kind, kind = 'X', 'N', 'T', 'INT'
-subset_dict = {'lon' : (40, 120)}
-subset = '_40E-120E_90S-90N'
-nperday = 24
+subset_dict = {'lon' : (lon1, lon2), 'lat' : (lat1, lat2)}
+if plev is not None:
+    subset_dict['plev'] = plev
+lonstr = atm.latlon_labels([lon1, lon2], 'lon', deg_symbol=False, join_str='-')
+latstr = atm.latlon_labels([lat1, lat2], 'lat', deg_symbol=False, join_str='-')
+subset = '_%s_%s' % (lonstr, latstr)
+if plev is not None:
+    subset = '%d' % plev + subset
+time_dim = {'merra' : 'TIME', 'merra2' : 'time'}[version]
 
+opts = {'UFLXQV' : ('X', 'N', 'T', 'INT'), 'VFLXQV' : ('X', 'N', 'T', 'INT'),
+        'UFLXCPT' : ('X', 'N', 'T', 'INT'), 'VFLXCPT' : ('X', 'N', 'T', 'INT'),
+        'UFLXPHI' : ('X', 'N', 'T', 'INT'), 'VFLXPHI' : ('X', 'N', 'T', 'INT'),
+        'DQVDT_ANA' : ('X', 'N', 'T', 'INT'), 'TQV' : ('X', 'N', 'I', 'INT'),
+        'PRECTOT' : ('X', 'N', 'T', 'FLX'), 'EVAP' : ('X', 'N', 'T', 'FLX')}
 
-def savefile(datadir, varnm, year, month, subset):
+def monthlyfile(datadir, varnm, year, month, subset):
     return '%smerra_%s%s_%d%02d.nc' % (datadir, varnm, subset, year, month)
 
 def yrlyfile(datadir, varnm, year, subset):
     return '%smerra_%s%s_%d.nc' % (datadir, varnm, subset, year)
 
 for varnm in varnms:
-    for year in years:
-        url_dict = merra.merra_urls([year], vertical, res, time_kind, kind)
+    vertical, res, time_kind, kind = opts[varnm]
+    for year in years:        
         for month in months:
+            url_dict = merra.merra_urls(year, month, version, vertical, res, 
+                                        time_kind, kind)
             days = range(1, atm.days_this_month(year, month) + 1)
             jdays = atm.season_days(atm.month_str(month), atm.isleap(year))
             urls = [url_dict['%d%02d%02d' % (year, month, day)] for day in days]
-            var = atm.load_concat(urls, varnm, concat_dim='TIME',
+            var = atm.load_concat(urls, varnm, concat_dim=time_dim,
                                   subset_dict=subset_dict)
+            nperday = len(var[time_dim]) / len(days)
             var = atm.daily_from_subdaily(var, nperday, dayname='day',
                                           dayvals=jdays)
-            filenm = savefile(datadir, varnm, year, month, subset)
+            filenm = monthlyfile(datadir, varnm, year, month, subset)
             print('Saving to ' + filenm)
             atm.save_nc(filenm, var)
 
         # Consolidate monthly files into yearly files
-        files = [savefile(datadir, varnm, year, m, subset) for m in months]
+        files = [monthlyfile(datadir, varnm, year, m, subset) for m in months]
         var = atm.load_concat(files, varnm, concat_dim='day')
         filenm = yrlyfile(datadir, varnm, year, subset)
         print('Saving to ' + filenm)
         atm.save_nc(filenm, var)
+        print('Deleting monthly files:')
+        for filenm in files:
+            print(filenm)
+            os.remove(filenm)
 
-# ----------------------------------------------------------------------
-# # Consolidate monthly files into yearly files
-#
-# def yrlyfile(datadir, varnm, year, subset):
-#     return '%smerra_%s%s_%d.nc' % (datadir, varnm, subset, year)
-#
-# for varnm in varnms:
-#     for year in years:
-#         files = [savefile(datadir, varnm, year, m, subset) for m in months]
-#         var = atm.load_concat(files, varnm, concat_dim='day')
-#         filenm = yrlyfile(datadir, varnm, year, subset)
-#         print('Saving to ' + filenm)
-#         atm.save_nc(filenm, var)
