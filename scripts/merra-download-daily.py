67d9b32f4dafaf9b1db8cf966ecd6346541b830b
Jennifer Walker <jenfly@gmail.com>
2016-03-03 11:57:21 -0800

    Re-organized to read multiple variables from url at once, write to temporary daily files and then consolidate to yearly files

--- scripts/merra-download-daily.py
+++ scripts/merra-download-daily.py
@@ -16,7 +16,8 @@ sys.path.append('/home/jwalker/dynamics/python/atmos-read')
 import os
 import xray
 import numpy as np
-from datetime import datetime
+import collections
+import time
 import matplotlib.pyplot as plt
 import pandas as pd
 import atmos as atm
@@ -35,7 +36,9 @@ years = [1980, 1981]
 datadir = atm.homedir() + 'datastore/' + version + '/daily/'
 months = np.arange(1, 13)
 
-varnms = ['U', 'PRECTOT', 'V']
+varnms = ['U', 'PRECTOT', 'V', 'TQV', 'EVAP', 'OMEGA']
+
+dp_vars = ['U', 'OMEGA']
 
 latlon=(-90, 90, 40, 120)
 plevs=(850, 200)
@@ -44,20 +47,40 @@ sector_lons=(60, 100)
 nc_fmt = {'merra' : None, 'merra2' : 'NETCDF4_classic'}[version]
 nc_eng = {'merra' : None, 'merra2' : 'netcdf4'}[version]
 
-def get_filename(var, version, datadir, year, month=None):
-    filenm = datadir + version + '_' + var.attrs['filestr']
+
+def group_variables(varnms, version):
+    """Group variables together according to URL."""
+    def get_group(varnm, version):
+        opts = merra.url_opts(varnm, version)
+        group = '%s%s_%s_%s' % (opts['res'], opts['vertical'], opts['kind'],
+                                opts['time_kind'])
+        return group
+
+    groups = {nm : get_group(nm, version) for nm in varnms}
+    keys = set(groups.values())
+    vargroups = collections.defaultdict(list)
+    for nm, key in groups.iteritems():
+        vargroups[key] += [nm]
+    return vargroups
+
+def get_filename(var, version, datadir, year, month=None, day=None):
+    """Return a filename for a variable."""
+    filenm = datadir + version + '_' + var.attrs['filestr'] + '_%d' % year
     if month is not None:
-        filenm = filenm + '_%d%02d.nc' % (year, month)
-    else:
-        filenm = filenm + '_%d.nc' % year
+        filenm = filenm + '%02d' % month
+    if day is not None:
+        filenm = filenm + '%02d' % day
+    filenm = filenm + '.nc'
     return filenm
 
 def latlon_filestr(lat1, lat2, lon1, lon2):
+    """Return nicely formatted string for lat-lon range."""
     latstr = atm.latlon_str(lat1, lat2, 'lat')
     lonstr = atm.latlon_str(lon1, lon2, 'lon')
     return lonstr + '_' + latstr
 
 def latlon_data(var, lat1, lat2, lon1, lon2, plev=None):
+    """Extract lat-lon subset of data."""
     name = var.name
     subset_dict = {'lat' : (lat1, lat2), 'lon' : (lon1, lon2)}
     latlonstr = latlon_filestr(lat1, lat2, lon1, lon2)
@@ -70,6 +93,7 @@ def latlon_data(var, lat1, lat2, lon1, lon2, plev=None):
     return var
 
 def pgradient(var, lat1, lat2, lon1, lon2, plev):
+    """Return d/dp of a lat-lon variable."""
     pwidth = 100
     p1, p2 = plev - pwidth, plev + pwidth
     var = atm.subset(var, {'lat' : (lat1, lat2), 'lon' : (lon1, lon2),
@@ -94,6 +118,7 @@ def pgradient(var, lat1, lat2, lon1, lon2, plev):
     return dvar_dp
 
 def sector_mean(var, lon1, lon2):
+    """Return the sector mean of a variable."""
     name = var.name
     lonstr = atm.latlon_str(lon1, lon2, 'lon')
     varbar = atm.dim_mean(var, 'lon', lon1, lon2)
@@ -103,15 +128,15 @@ def sector_mean(var, lon1, lon2):
     varbar.attrs['filestr'] = '%s_sector_%s' % (name, lonstr)
     return varbar
 
-def calc_data(var, jday=0, latlon=(-90, 90, 40, 120), plevs=(850, 200),
-             sector_lons=(60, 100)):
-
+def var_calcs(var, jday=0, latlon=(-90, 90, 40, 120), plevs=(850, 200),
+              dp_vars=['U', 'OMEGA'], sector_lons=(60, 100)):
+    """Process a single variable from a single day."""
     lat1, lat2, lon1, lon2 = latlon
     opts = merra.url_opts(var.name)
     vertical = opts['vertical']
     if vertical == 'X':
         plevs = [None]
-    if var.name in ['U', 'OMEGA']:
+    if dp_vars is not None and var.name in dp_vars:
         dp = True
     else:
         dp = False
@@ -142,54 +167,139 @@ def calc_data(var, jday=0, latlon=(-90, 90, 40, 120), plevs=(850, 200),
     data = atm.daily_from_subdaily(data, nperday, dayname='day',
                                    dayvals=[jday])
 
+    # Make sure output is in a Dataset
+    if isinstance(data, xray.DataArray):
+        data = data.to_dataset()
+
     return data
 
+def all_data(ds, varnms, datadir, year, month, day, jday, calc_kw, nc_kw):
+    """Process selected variables in a dataset and save each to file."""
+    files = {}
+    for nm in varnms:
+        print(nm)
+        data = var_calcs(ds[nm], jday, **calc_kw)
+        filenm = '%s%s_%d%02d%02d.nc' % (datadir, nm, year, month, day)
+        print('Saving to ' + filenm)
+        data.to_netcdf(filenm, **nc_kw)
+        files[nm] = filenm
+    return files
 
-def get_kw(jdays, latlon, plevs, sector_lons):
-    kw = [{'jday' : jday, 'latlon' : latlon, 'plevs' : plevs,
-           'sector_lons' : sector_lons} for jday in jdays]
-    return kw
+def read_url(url, varnms, datadir, year, month, day, jday, calc_kw, nc_kw):
+    """Open url and process selected variables."""
+    # Number of times to attempt opening url (in case of server problems)
+    NMAX = 3
+    # Wait time (seconds) between attempts
+    WAIT = 5
 
+    print('Loading ' + url)
+    attempt = 0
+    while attempt < NMAX:
+        try:
+            with xray.open_dataset(url) as ds:
+                files = all_data(ds, varnms, datadir, year, month, day, jday,
+                                 calc_kw, nc_kw)
+            attempt = NMAX
+        except RuntimeError as err:
+            attempt += 1
+            if attempt < NMAX:
+                print('Error reading file.  Attempting again in %d s' % WAIT)
+                time.sleep(WAIT)
+            else:
+                raise err
+    return files
+
+def read_groups(url_dict, vargroups, datadir, year, month, day, jday, calc_kw,
+                nc_kw):
+    """Process variables for a day, grouped by URL."""
+    files = {}
+    for key, varids in vargroups.iteritems():
+        url = url_dict[key]['%d%02d%02d' % (year, month, day)]
+        datafiles = read_url(url, varids, datadir, year, month, day, jday,
+                             calc_kw, nc_kw)
+        files.update(datafiles)
+    return files
+
+def get_url_dict(year, month, version, vargroups):
+    """Return dict of urls for the variable groups."""
+    url_dict = {}
+    for key in vargroups:
+        nm = vargroups[key][0]
+        url_dict[key] = merra.get_urls(year, month, version, nm)
+    return url_dict
+
+# Initial setup
+vargroups = group_variables(varnms, version)
+calc_kw = {'latlon' : latlon, 'plevs' : plevs, 'dp_vars' : dp_vars,
+           'sector_lons' : sector_lons}
+nc_kw = {'format' : nc_fmt, 'engine' : nc_eng}
 
 # Read data and concatenate
 for year in years:
-    for varnm in varnms:
-        for month in months:
-            url_dict = merra.get_urls(year, month, version, varnm)
-            days = range(1, atm.days_this_month(year, month) + 1)
-            jdays = atm.season_days(atm.month_str(month), atm.isleap(year))
-            urls = [url_dict['%d%02d%02d' % (year, month, day)] for day in days]
-            func_kw = get_kw(jdays, latlon, plevs, sector_lons)
-            data = atm.load_concat(urls, varnm, concat_dim='day', func=calc_data,
-                                   func_kw=func_kw)
-            if isinstance(data, xray.DataArray):
-                data = data.to_dataset()
-
-            # Save monthly files
-
-            for nm in data.data_vars:
-                var = data[nm]
-                filenm = get_filename(var, version, datadir, year, month)
-
-                print('Saving to ' + filenm)
-                ds = var.to_dataset()
-                ds.to_netcdf(filenm, format=nc_fmt, engine=nc_eng)
-
-                # Check if output is corrupted
-                with xray.open_dataset(filenm) as ds_check:
-                    print(ds.dims.keys())
-                    print(ds.data_vars.keys())
-                    if len(ds.data_vars.keys()) > 1:
-                        raise ValueError('Corrupted monthly output file')
-
-        # Consolidate monthly files into yearly files
-        for nm in data.data_vars:
-            files = [get_filename(data[nm], version, datadir, year, m) for m in months]
-            var = atm.load_concat(files, nm, concat_dim='day')
-            filenm = get_filename(var, version, datadir, year)
+    dailyfiles = collections.defaultdict(list)
+    for month in months:
+        url_dict = get_url_dict(year, month, version, vargroups)
+        days = range(1, atm.days_this_month(year, month) + 1)
+        jdays = atm.season_days(atm.month_str(month), atm.isleap(year))
+        for day, jday in zip(days, jdays):
+            files = read_groups(url_dict, vargroups, datadir, year, month, day,
+                                jday, calc_kw, nc_kw)
+            for nm in files:
+                dailyfiles[nm] += [files[nm]]
+    # Consolidate daily files into yearly files and delete daily files
+    for nm in dailyfiles:
+        data = atm.load_concat(dailyfiles[nm], concat_dim='day')
+        for varnm in data.data_vars:
+            filenm = get_filename(data[varnm], version, datadir, year)
             print('Saving to ' + filenm)
-            var.to_dataset().to_netcdf(filenm)
-            print('Deleting monthly files:')
-            for filenm in files:
-                print(filenm)
-                os.remove(filenm)
+            atm.save_nc(filenm, data[varnm])
+        print('Deleting daily files'):
+        for filenm in dailyfiles[nm]:
+            print(filenm)
+            os.remove(filenm)
+
+
+
+# ----------------------------------------------------------------------
+# Read data and concatenate
+# for year in years:
+#     for varnm in varnms:
+#         for month in months:
+#             url_dict = merra.get_urls(year, month, version, varnm)
+#             days = range(1, atm.days_this_month(year, month) + 1)
+#             jdays = atm.season_days(atm.month_str(month), atm.isleap(year))
+#             urls = [url_dict['%d%02d%02d' % (year, month, day)] for day in days]
+#             func_kw = get_kw(jdays, latlon, plevs, sector_lons)
+#             data = atm.load_concat(urls, varnm, concat_dim='day', func=calc_data,
+#                                    func_kw=func_kw)
+#             if isinstance(data, xray.DataArray):
+#                 data = data.to_dataset()
+#
+#             # Save monthly files
+#
+#             for nm in data.data_vars:
+#                 var = data[nm]
+#                 filenm = get_filename(var, version, datadir, year, month)
+#
+#                 print('Saving to ' + filenm)
+#                 ds = var.to_dataset()
+#                 ds.to_netcdf(filenm, format=nc_fmt, engine=nc_eng)
+#
+#                 # Check if output is corrupted
+#                 with xray.open_dataset(filenm) as ds_check:
+#                     print(ds.dims.keys())
+#                     print(ds.data_vars.keys())
+#                     if len(ds.data_vars.keys()) > 1:
+#                         raise ValueError('Corrupted monthly output file')
+#
+#         # Consolidate monthly files into yearly files
+#         for nm in data.data_vars:
+#             files = [get_filename(data[nm], version, datadir, year, m) for m in months]
+#             var = atm.load_concat(files, nm, concat_dim='day')
+#             filenm = get_filename(var, version, datadir, year)
+#             print('Saving to ' + filenm)
+#             var.to_dataset().to_netcdf(filenm)
+#             print('Deleting monthly files:')
+#             for filenm in files:
+#                 print(filenm)
+#                 os.remove(filenm)
