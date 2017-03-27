b9676e55c63350403b90f9056fbe359f852603f4
Jennifer Walker <jenfly@gmail.com>
2017-03-26 20:49:14 -0700

    Testing with new versions of pydap and xarray mods

--- testing/merra2-new-download.py
+++ testing/merra2-new-download.py
@@ -9,15 +9,15 @@ import numpy as np
 import collections
 import atmos as atm
 
-url = ('https://goldsmr5.gesdisc.eosdis.nasa.gov/opendap/hyrax/MERRA2/'
-       'M2I3NVASM.5.12.4/2015/08/MERRA2_400.inst3_3d_asm_Nv.20150802.nc4')
-
-url2 = ('https://goldsmr4.gesdisc.eosdis.nasa.gov/opendap/MERRA2/'
-        'M2I1NXASM.5.12.4/2015/08/MERRA2_400.inst1_2d_asm_Nx.20150802.nc4')
-
 from pydap.client import open_url
 from pydap.cas.urs import setup_session
 netrc_file = atm.homedir() + '.netrc'
+
+url_3d = ('https://goldsmr5.gesdisc.eosdis.nasa.gov/opendap/hyrax/MERRA2/'
+         'M2I3NVASM.5.12.4/2015/08/MERRA2_400.inst3_3d_asm_Nv.20150802.nc4')
+
+url_2d = ('https://goldsmr4.gesdisc.eosdis.nasa.gov/opendap/MERRA2/'
+          'M2I1NXASM.5.12.4/2015/08/MERRA2_400.inst1_2d_asm_Nx.20150802.nc4')
 
 def get_login(netrc_file):
     with open(netrc_file) as f:
@@ -28,15 +28,27 @@ def get_login(netrc_file):
     return auth
 
 auth = get_login(netrc_file)
+username, password = auth['username'], auth['password']
 
-session = setup_session(auth['username'], auth['password'], check_url=url)
-session2 = setup_session(auth['username'], auth['password'], check_url=url2)
-ds = open_url(url, session=session)
-ds2 = open_url(url2, session=session2)
+def read_file(url, username, password):
+    session = setup_session(username, password, check_url=url)
+    ds = open_url(url, session=session)
+    return ds
+
+ds_3d = read_file(url_3d, username, password)
+ds_2d = read_file(url_2d, username, password)
 
 
-print(ds['T'].shape)
-print(ds['T'][0, 0, 0, 0])
-print(ds2['TS'].shape)
-print(ds2['TS'][0, 0, 0])
+print(ds_3d['T'].shape)
+print(ds_3d['T'][0, 0, 0, 0])
+print(ds_2d['TS'].shape)
+print(ds_2d['TS'][0, 0, 0])
 
+#url = 'http://iridl.ldeo.columbia.edu/SOURCES/.OSU/.PRISM/.monthly/dods'
+url = 'http://test.opendap.org/dap/data/nc/coads_climatology.nc'
+ds = xray.open_dataset(url, engine='pydap', decode_times=False)
+
+url3 = url_3d
+session = setup_session(username, password, check_url=url3)
+store = xray.backends.PydapDataStore(url3, session=session)
+ds = xray.open_dataset(store, decode_times=False, decode_coords=False)
