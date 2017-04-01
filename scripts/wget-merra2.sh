e55025443b9a71a7b5bced141f33aa10ac38d16f
Jennifer Walker <jenfly@gmail.com>
2017-03-31 18:38:32 -0700

    Download files with wget

--- scripts/wget-merra2.sh
+++ scripts/wget-merra2.sh
@@ -1,15 +1,21 @@
 #! /bin/bash
 
-cd /home/jwalker/datastore/merra2/dailyrad/
 
-for yr in {1980..2015}
-do
-    mkdir $yr
-    cd $yr
-    filenm=/home/jwalker/dynamics/python/atmos-read/scripts/merra_urls/merra2_rad_$yr.txt
-    wget  --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --keep-session-cookie --no-check-certificate --content-disposition -i $filenm
-    cd ..
-done
+cd /net/eady/data1/jwalker/datastore/merra2/wget
+filenm=/net/eady/data1/jwalker/dynamics/python/atmos-read/scripts/merra_urls/merra_urls_ubudget.txt
+wget  --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --keep-session-cookie --no-check-certificate --content-disposition -i $filenm
+
+# ------------------------------
+#cd /home/jwalker/datastore/merra2/dailyrad/
+#
+#for yr in {1980..2015}
+#do
+#    mkdir $yr
+#    cd $yr
+#    filenm=/home/jwalker/dynamics/python/atmos-read/scripts/merra_urls/merra2_rad_$yr.txt
+#    wget  --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --keep-session-cookie --no-check-certificate --content-disposition -i $filenm
+#    cd ..
+#done
 # ------------------------------
 
 # url="http://goldsmr4.gesdisc.eosdis.nasa.gov/daac-bin/OTF/HTTP_services.cgi?FILENAME=%2Fdata%2FMERRA2%2FM2T1NXRAD.5.12.4%2F1980%2F01%2FMERRA2_100.tavg1_2d_rad_Nx.19800101.nc4&FORMAT=bmM0Lw&BBOX=-65%2C40%2C65%2C120&LABEL=MERRA2_100.tavg1_2d_rad_Nx.19800101.SUB.nc4&FLAGS=1&SHORTNAME=M2T1NXRAD&SERVICE=SUBSET_MERRA2&LAYERS=&VERSION=1.02&VARIABLES=lwgnt%2Clwtup%2Cswgnt%2Cswtnt"
